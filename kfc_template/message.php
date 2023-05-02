52db5acbb38b31ca488b2058dac204d9b8aa9d8f
Shuya <takeuchi.0124s@gmail.com>
2023-05-02 12:17:48 +0900

    no message

--- kfc_template/message.php
+++ kfc_template/message.php
@@ -6,14 +6,20 @@ require_once("./lib/util.php");
 $pagetitle = "メッセージ"
 ?>
 <?php include('parts/header_message.php'); ?>
-<div id="container">
+
+
+<div id="container" class="c1" style="display:block">
   <main>
     <?php
-    // 現在ログインしているユーザー情報
-    $_SESSION['user_id'] = 1;
-    $current_user = get_user($_SESSION['user_id']);
     // メッセージ送信先のユーザー情報
-    $_GET['user_id'] = 2;
+    if (!isset($_GET['user_id'])) {
+      echo "送信先ユーザーが未選択です<br>";
+      echo "<a href='message_top.php'><button>戻る</button></a>";
+      exit();
+    }
+    // 現在ログインしているユーザー情報
+    $current_user = get_user($_SESSION['user_id']);
+
     $destination_user = get_user($_GET['user_id']);
     // やり取りされるメッセージ情報
     $messages = get_messages($current_user['user_id'], $destination_user['user_id']);
@@ -21,10 +27,17 @@ $pagetitle = "メッセージ"
 
     <body>
       <div class="message">
-        <div class="message_top_btn">
-          <a href="message_top.php">&lt;トークルーム一覧へ</a>
+        <div class="user_flex">
+          <div class="back_btn">
+            <a href="message_top.php"><button>＜ 戻る</button></a>
+          </div>
+          <div class="user_name">
+            <h2><?= $destination_user['user_name'] ?></h2>
+          </div>
+          <div class="textarea_btn">
+          <a href="#text4"><div class="arrow"></div></a>
+          </div>
         </div>
-        <h2 class="center"><?= $destination_user['user_name'] ?></h2>
         <?php foreach ($messages as $message) : ?>
           <div class="my_message">
             <?php if ($message['user_id'] == $current_user['user_id']) : ?>
@@ -33,28 +46,52 @@ $pagetitle = "メッセージ"
                 <p><?= $message['text'] ?></p>
               </div>
             <?php else : ?>
-              <div class="left">
-                <div class="says"><?= $message['text'] ?></div><span class="message_created_at"><?= convert_to_fuzzy_time($message['created_at']) ?></span>
-              <?php endif; ?>
+              <div class="left"><img src="./images/足跡アイコン.png" class="icon_image">
+                <div class="says">
+                <p><?= $message['text'] ?></p>
+                </div>
+                <span class="message_created_at"><?= convert_to_fuzzy_time($message['created_at']) ?>
+                </span>
               </div>
-            <?php endforeach ?>
+            <?php endif; ?>
+          <?php endforeach ?>
           </div>
           <div class="message_process">
-            <h2 class="message_title">メッセージ</h2>
-
             <!-- message_add.phpにPOSTするフォーム -->
             <form method="post" action="./message_add.php">
-              <textarea class="textarea form-control" placeholder="メッセージを入力ください" name="text"></textarea>
+              <textarea class="textarea form-control" placeholder="メッセージを入力ください" name="text" id="text4" autocomplete="off"></textarea>
               <input type="hidden" name="destination_user_ID" value="<?= $destination_user['user_id']; ?>">
               <div class="message_btn">
-                <div class="message_image">
-                  <!-- <input type="file" name="image" class="my_image" accept="image/*" multiple> -->
-                </div>
-                <button class="btn btn-outline-primary" type="submit" name="post" value="post" id="post">投稿</button>
+                <button class="btn btn-outline-primary btn_03" type="submit" name="post" value="post" id="post">送信</button>
               </div>
             </form>
           </div>
+      </div>
 
+      <script>
+        // テキストエリア入力したら送信ボタン有効
+        window.addEventListener('DOMContentLoaded', function() {
+          document.getElementById('post').disabled = true;
+          document.getElementById('text4').addEventListener('keyup', function() {
+            if (this.value.length < 1) {
+              document.getElementById('post').disabled = true;
+            } else {
+              document.getElementById('post').disabled = false;
+            }
+          }, false);
+
+          // postしたら*ms後にdisabledにする
+          let btn = document.getElementById('post');
+          btn.addEventListener('click', function() {
+            window.setTimeout(click_disabled, 10);
+          });
+          //post無効の自作関数
+          function click_disabled() {
+            document.getElementById('post').disabled = true;
+          }
+
+        }, false);
+      </script>
     </body>
 
     <?php
@@ -67,17 +104,15 @@ $pagetitle = "メッセージ"
         $host = 'localhost';
         $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
         $dbh = new PDO($dsn, $user, $password);
-        $sql = "SELECT user_id,user_name,password FROM user
-                      WHERE user_id = :id";
-        // $sql = "SELECT user_id,user_name,password FROM user
-        //         WHERE user_id = :id AND delete_flg = 0 ";
+        $sql = "SELECT user_id,user_name,password 
+        FROM user 
+        WHERE user_id = :id";
         $stmt = $dbh->prepare($sql);
         $stmt->bindValue(':id', $user_id, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetch();
       } catch (\Exception $e) {
         error_log('エラー発生:' . $e->getMessage());
-        // set_flash('error', ERR_MSG1);
         echo "ERR_MSG1";
       }
     }
@@ -103,12 +138,8 @@ $pagetitle = "メッセージ"
         $stmt->bindValue(':destination_user_id_2', $destination_user_id, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetchAll();
-
-        // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // var_dump($result);
       } catch (\Exception $e) {
         error_log('エラー発生:' . $e->getMessage());
-        // set_flash('error', ERR_MSG1);
         echo "ERR_MSG1";
       }
     }
@@ -117,15 +148,6 @@ $pagetitle = "メッセージ"
 
 
     <?php
-    /**
-     * X秒前、X分前、X時間前、X日前などといった表示へ変換
-     * 一分未満は秒、一時間未満は分、一日未満は時間、
-     * 31日以内はX日前、それ以上はX月X日と返す
-     * X月X日表記の時、年が異なる場合はyyyy年m月d日と、年も表示する
-     *
-     * @param   <String> $time_db       strtotime()で変換できる時間文字列 (例：yyyy/mm/dd H:i:s)
-     * @return  <String>                X日前,などといった文字列
-     **/
     function convert_to_fuzzy_time($time_db)
     {
       date_default_timezone_set('Asia/Tokyo');
--- kfc_template/message_top.php
+++ kfc_template/message_top.php
@@ -7,11 +7,13 @@ require_once("./lib/util.php");
 $pagetitle = "トークルーム一覧"
 ?>
 <?php include('parts/header_message.php'); ?>
-<div id="container" class='c1'>
+<div id="container" class='c1' style="display:block">
   <main>
+    <div class="mypage_btn">
+      <a href="message_top.php"><button>＜マイページに戻る</button></a>
+    </div>
     <?php
-
-    $destination_user = get_user(2); //$_GET['user_id']
+    $_SESSION['user_id'] = 2;
     $current_user = get_user($_SESSION['user_id']);
     $message_relations = get_message_relations($current_user['user_id']);
 
@@ -25,15 +27,18 @@ $pagetitle = "トークルーム一覧"
     ?>
 
       <body>
+
         <div class="row">
           <div class="col-8 offset-2">
             <a href='message.php?user_id=<?= $destination_user['user_id'] ?>'>
               <div class="destination_user_list">
+                <div class="icon">
+                  <img src="./images/足跡アイコン.png" alt="アイコン" class="icon_image">
+                </div>
                 <div class='destination_user_info'>
                   <div class="destination_user_name"><?= $destination_user['user_name'] ?></div>
                   <span class="destination_user_text"><?= $bottom_message['text'] ?></span>
                 </div>
-
                 <span class="bottom_message_time"><?= convert_to_fuzzy_time($bottom_message['created_at']); ?></span>
               </div>
             </a>
@@ -68,7 +73,6 @@ $pagetitle = "トークルーム一覧"
           return $stmt->fetch();
         } catch (\Exception $e) {
           error_log('エラー発生:' . $e->getMessage());
-          // set_flash('error', ERR_MSG1);
           echo "ERR_MSG1";
         }
       }
@@ -118,15 +122,12 @@ $pagetitle = "トークルーム一覧"
           $dbh = new PDO($dsn, $user, $password);
           $sql = "SELECT user_id,user_name,password FROM user
                             WHERE user_id = :id";
-          // $sql = "SELECT user_id,user_name,password FROM user
-          //         WHERE user_id = :id AND delete_flg = 0 ";
           $stmt = $dbh->prepare($sql);
           $stmt->bindValue(':id', $user_id, PDO::PARAM_STR);
           $stmt->execute();
           return $stmt->fetch();
         } catch (\Exception $e) {
           error_log('エラー発生:' . $e->getMessage());
-          // set_flash('error', ERR_MSG1);
           echo "ERR_MSG1";
         }
       }
@@ -151,7 +152,6 @@ $pagetitle = "トークルーム一覧"
           return $result;
         } catch (\Exception $e) {
           error_log('エラー発生:' . $e->getMessage());
-          // set_flash('error', ERR_MSG1);
           echo "ERR_MSG1";
         }
       }
