4ad36446ba4e34c199d6f12c678218e28b459a5f
Shuya <takeuchi.0124s@gmail.com>
2023-05-08 11:49:46 +0900

    no message

--- kfc_template/message.php
+++ kfc_template/message.php
@@ -72,181 +72,181 @@ $pagetitle = "メッセージ"
         </form>
       </div>
       </div>
-      </div>
-      <script>
-        // テキストエリア入力したら送信ボタン有効
-        window.addEventListener('DOMContentLoaded', function() {
-          document.getElementById('post').disabled = true;
-          document.getElementById('text4').addEventListener('keyup', function() {
-            if (this.value.length < 1) {
-              document.getElementById('post').disabled = true;
-            } else {
-              document.getElementById('post').disabled = false;
-            }
-          }, false);
+</div>
+<script>
+  // テキストエリア入力したら送信ボタン有効
+  window.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('post').disabled = true;
+    document.getElementById('text4').addEventListener('keyup', function() {
+      if (this.value.length < 1) {
+        document.getElementById('post').disabled = true;
+      } else {
+        document.getElementById('post').disabled = false;
+      }
+    }, false);
 
-          // postしたら*ms後にdisabledにする
-          let btn = document.getElementById('post');
-          btn.addEventListener('click', function() {
-            window.setTimeout(click_disabled, 10);
-          });
-          //post無効の自作関数
-          function click_disabled() {
-            document.getElementById('post').disabled = true;
-          }
-        }, false);
-      </script>
-    </body>
+    // postしたら*ms後にdisabledにする
+    let btn = document.getElementById('post');
+    btn.addEventListener('click', function() {
+      window.setTimeout(click_disabled, 10);
+    });
+    //post無効の自作関数
+    function click_disabled() {
+      document.getElementById('post').disabled = true;
+    }
+  }, false);
+</script>
+</body>
 
-    <?php
-    function get_user($user_id)
-    {
-      try {
-        $user = 'shotohlcd31_kfc';
-        $password = 'KFCpassword';
-        $dbName = 'shotohlcd31_kfc';
-        $host = 'localhost';
-        $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
-        $dbh = new PDO($dsn, $user, $password);
-        $sql = "SELECT user_id,user_name,password 
+<?php
+function get_user($user_id)
+{
+  try {
+    $user = 'shotohlcd31_kfc';
+    $password = 'KFCpassword';
+    $dbName = 'shotohlcd31_kfc';
+    $host = 'localhost';
+    $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
+    $dbh = new PDO($dsn, $user, $password);
+    $sql = "SELECT user_id,user_name,password 
         FROM user 
         WHERE user_id = :id";
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':id', $user_id, PDO::PARAM_STR);
-        $stmt->execute();
-        return $stmt->fetch();
-      } catch (\Exception $e) {
-        error_log('エラー発生:' . $e->getMessage());
-        echo "ERR_MSG1";
-      }
-    }
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':id', $user_id, PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt->fetch();
+  } catch (\Exception $e) {
+    error_log('エラー発生:' . $e->getMessage());
+    echo "ERR_MSG1";
+  }
+}
 
 
-    function get_messages($user_id, $destination_user_id)
-    {
-      try {
-        $user = 'shotohlcd31_kfc';
-        $password = 'KFCpassword';
-        $dbName = 'shotohlcd31_kfc';
-        $host = 'localhost';
-        $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
-        $dbh = new PDO($dsn, $user, $password);
-        $sql = "SELECT *
+function get_messages($user_id, $destination_user_id)
+{
+  try {
+    $user = 'shotohlcd31_kfc';
+    $password = 'KFCpassword';
+    $dbName = 'shotohlcd31_kfc';
+    $host = 'localhost';
+    $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
+    $dbh = new PDO($dsn, $user, $password);
+    $sql = "SELECT *
                       FROM message
                       WHERE (user_id = :id_1 and destination_user_id = :destination_user_id_1) or (user_id = :destination_user_id_2 and destination_user_id = :id_2)
                       ORDER BY created_at ASC";
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':id_1', $user_id, PDO::PARAM_INT);
-        $stmt->bindValue(':destination_user_id_1', $destination_user_id, PDO::PARAM_INT);
-        $stmt->bindValue(':id_2', $user_id, PDO::PARAM_INT);
-        $stmt->bindValue(':destination_user_id_2', $destination_user_id, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->fetchAll();
-      } catch (\Exception $e) {
-        error_log('エラー発生:' . $e->getMessage());
-        echo "ERR_MSG1";
-      }
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':id_1', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':destination_user_id_1', $destination_user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':id_2', $user_id, PDO::PARAM_INT);
+    $stmt->bindValue(':destination_user_id_2', $destination_user_id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll();
+  } catch (\Exception $e) {
+    error_log('エラー発生:' . $e->getMessage());
+    echo "ERR_MSG1";
+  }
+}
+
+?>
+
+
+<?php
+function convert_to_fuzzy_time($time_db)
+{
+  date_default_timezone_set('Asia/Tokyo');
+
+  $unix   = strtotime($time_db);
+  $now    = time();
+  $diff_sec   = $now - $unix;
+
+  if ($diff_sec < 60) {
+    $time   = $diff_sec;
+    $unit   = "秒前";
+  } elseif ($diff_sec < 3600) {
+    $time   = $diff_sec / 60;
+    $unit   = "分前";
+  } elseif ($diff_sec < 86400) {
+    $time   = $diff_sec / 3600;
+    $unit   = "時間前";
+  } elseif ($diff_sec < 2764800) {
+    $time   = $diff_sec / 86400;
+    $unit   = "日前";
+  } else {
+    if (date("Y") != date("Y", $unix)) {
+      $time   = date("Y年n月j日", $unix);
+    } else {
+      $time   = date("n月j日", $unix);
     }
 
-    ?>
+    return $time;
+  }
 
+  return (int)$time . $unit;
+}
+?>
+<?php
+// 里親申し込み完了後のトークルーム作成
 
-    <?php
-    function convert_to_fuzzy_time($time_db)
-    {
-      date_default_timezone_set('Asia/Tokyo');
-
-      $unix   = strtotime($time_db);
-      $now    = time();
-      $diff_sec   = $now - $unix;
-
-      if ($diff_sec < 60) {
-        $time   = $diff_sec;
-        $unit   = "秒前";
-      } elseif ($diff_sec < 3600) {
-        $time   = $diff_sec / 60;
-        $unit   = "分前";
-      } elseif ($diff_sec < 86400) {
-        $time   = $diff_sec / 3600;
-        $unit   = "時間前";
-      } elseif ($diff_sec < 2764800) {
-        $time   = $diff_sec / 86400;
-        $unit   = "日前";
-      } else {
-        if (date("Y") != date("Y", $unix)) {
-          $time   = date("Y年n月j日", $unix);
-        } else {
-          $time   = date("n月j日", $unix);
-        }
-
-        return $time;
-      }
-
-      return (int)$time . $unit;
-    }
-    ?>
-    <?php
-    // 里親申し込み完了後のトークルーム作成
-
-    $_POST = es($_POST);
-    $user_id = $_SESSION['user_id'];
-    $destination_user_ID = $_GET['user_id'];
-    $user_id = htmlspecialchars($user_id, ENT_QUOTES, 'UTF-8');
+$_POST = es($_POST);
+$user_id = $_SESSION['user_id'];
+$destination_user_ID = $_GET['user_id'];
+$user_id = htmlspecialchars($user_id, ENT_QUOTES, 'UTF-8');
 var_dump($user_id);
 var_dump($destination_user_ID);
-    if (!check_relation_message($user_id, $destination_user_ID)) {
-      insert_message($user_id, $destination_user_ID);
-      echo "relation_messageにデータを挿入";
-    }
-    ?>
-    <?php
-    function insert_message($user_id, $destination_user_ID)
-    {
-      try {
-        $user = 'shotohlcd31_kfc';
-        $password = 'KFCpassword';
-        $dbName = 'shotohlcd31_kfc';
-        $host = 'localhost';
-        $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
-        $dbh = new PDO($dsn, $user, $password);
-        $sql = "INSERT INTO message_relation(user_id,destination_user_id) VALUES (:user_id,:destination_user_ID)";
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
-        $stmt->bindValue(':destination_user_ID', $destination_user_ID, PDO::PARAM_STR);
-        $stmt->execute();
-      } catch (\Exception $e) {
-        error_log('エラー発生:' . $e->getMessage());
-        echo "ERR_MSG1";
-      }
-    }
+if (!check_relation_message($user_id, $destination_user_ID)) {
+  insert_message($user_id, $destination_user_ID);
+  echo "relation_messageにデータを挿入";
+}
+?>
+<?php
+function insert_message($user_id, $destination_user_ID)
+{
+  try {
+    $user = 'shotohlcd31_kfc';
+    $password = 'KFCpassword';
+    $dbName = 'shotohlcd31_kfc';
+    $host = 'localhost';
+    $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
+    $dbh = new PDO($dsn, $user, $password);
+    $sql = "INSERT INTO message_relation(user_id,destination_user_id) VALUES (:user_id,:destination_user_ID)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+    $stmt->bindValue(':destination_user_ID', $destination_user_ID, PDO::PARAM_STR);
+    $stmt->execute();
+  } catch (\Exception $e) {
+    error_log('エラー発生:' . $e->getMessage());
+    echo "ERR_MSG1";
+  }
+}
 
-    function check_relation_message($user_id, $destination_user_ID)
-    {
-      try {
-        $user = 'shotohlcd31_kfc';
-        $password = 'KFCpassword';
-        $dbName = 'shotohlcd31_kfc';
-        $host = 'localhost';
-        $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
-        $dbh = new PDO($dsn, $user, $password);
-        $sql = "SELECT user_id,destination_user_id
+function check_relation_message($user_id, $destination_user_ID)
+{
+  try {
+    $user = 'shotohlcd31_kfc';
+    $password = 'KFCpassword';
+    $dbName = 'shotohlcd31_kfc';
+    $host = 'localhost';
+    $dsn = "mysql:host={$host}; dbname={$dbName}; charset=utf8";
+    $dbh = new PDO($dsn, $user, $password);
+    $sql = "SELECT user_id,destination_user_id
             FROM message_relation
             WHERE (user_id = :user_id and destination_user_id = :destination_user_id)
                   or (user_id = :destination_user_id and destination_user_id = :user_id)";
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute(array(
-          ':user_id' => $user_id,
-          ':destination_user_id' => $destination_user_ID
-        ));
-        return $stmt->fetch();
-      } catch (\Exception $e) {
-        error_log('エラー発生:' . $e->getMessage());
-      }
-      echo "ERR_MSG1";
-    }
-    ?>
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute(array(
+      ':user_id' => $user_id,
+      ':destination_user_id' => $destination_user_ID
+    ));
+    return $stmt->fetch();
+  } catch (\Exception $e) {
+    error_log('エラー発生:' . $e->getMessage());
+  }
+  echo "ERR_MSG1";
+}
+?>
 
-  </main>
+</main>
 </div>
 
 <?php include('parts/footer.php'); ?>
